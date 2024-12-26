563be9d358b3dc06df6999fe89823f7a61abb29b
Champ <champsrw49@gmail.com>
2024-12-26 22:13:42 +0700

    lab12-2 complete

--- lab12_2.cpp
+++ lab12_2.cpp
@@ -1,9 +1,22 @@
 #include<iostream>
 using namespace std;
 
+int gcd(int x,int y);
+    
 int main(){
     int x, y;
     cin >> x >> y;
     cout << gcd(x,y);
     return 0; 
+}
+
+int gcd(int x,int y){
+    
+    while (y != 0){
+        int something = y;
+        y = x % y;
+        x = something;
+    }
+    
+    return x;
 }
