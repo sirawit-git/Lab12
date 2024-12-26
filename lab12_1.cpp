18039e415c91e1657adeeb02ab76ea5375785ca6
Champ <champsrw49@gmail.com>
2024-12-26 22:12:48 +0700

    ohno

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,19 +1,10 @@
 #include<iostream>
 using namespace std;
 
-int gcd(int x,int y){
-    while (y != 0){
-        int remainder = x % y;
-        
-        x = y;
-        y = remainder;
-    }
-    
-    return x;
-}
 int main(){
-    int x, y;
-    cin >> x >> y;
-    cout << gcd(x,y);
+    int x;
+    cin >> x;
+    cout << fibonacci(x);
     return 0; 
 }
+
